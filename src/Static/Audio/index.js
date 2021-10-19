f5e0dd506ebc4a3c0980ac99a0c67fc94240ce4f
User <User@DESKTOP-CT4MMP5>
2021-10-19 13:01:55 +0300

    Changed audio files to fixed ones.

--- src/Static/Audio/index.js
+++ src/Static/Audio/index.js
@@ -1,24 +1,24 @@
-import future_funk_beats from './future_funk_beats.mp3';
-import shutter_breakbeats from './shutter_breakbeats.mp3';
-import worwick_bass from './warwick_bass.mp3';
-import country_electric_guitar from './country_electric_guitar.mp3';
-import stompy_slosh from './stompy_slosh.mp3';
-import tangu_groove from './tangu_groove.mp3';
-import maze_politics from './maze_politics.mp3';
-import pase_groove from './pase_groove.mp3';
-import silent_star from './silent_star.mp3';
+import bass from './Bass.mp3';
+import drum from './Drum.mp3';
+import electric from './Electric.mp3';
+import future_funk from './Future_Funk.mp3';
+import organ from './Organ.mp3';
+import perq from './Perq.mp3';
+import stomp from './Stomp.mp3';
+import shutter from './Shutter.mp3';
+import weird from './Weird.mp3';
 import metronome from './metronome_120BPM.mp3';
 
 const audioArray = [
-    future_funk_beats,
-    shutter_breakbeats,
-    worwick_bass,
-    country_electric_guitar,
-    stompy_slosh,
-    tangu_groove,
-    maze_politics,
-    pase_groove,
-    silent_star
+    future_funk,
+    shutter,
+    bass,
+    electric,
+    stomp,
+    perq,
+    weird,
+    drum,
+    organ
 ]
 
 export{
